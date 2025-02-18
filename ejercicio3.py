454fa8d37b36e2cdb9bb86122ed1d45f5ae11fb7
jvdevsec <asahi_010101@protonmail.com>
2025-02-18 03:50:02 -0500

    ejercicio rombicosidodecaedro

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+
+lado = 3
+raiz_cuadrada3 = 3 ** (1/2)  # √3
+raiz_cuadrada5 = 5 ** (1/2)  # √5
+termino1 = (25 + 10 * raiz_cuadrada5) ** (1/2)  # √(25 + 10√5)
+    
+area = (30 + 5 * raiz_cuadrada3 + 3 * termino1) * lado**2
+
+print(f"El área superficial del rombicosidodecaedro con lado {lado} es: {area}")
